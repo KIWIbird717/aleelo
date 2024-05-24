47cd8f46445434c458ed5909c6b77e5fda51fc5f
Evgeniy Trubnikov <parmesanshees@mail.ru>
2024-05-24 04:37:40 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/shared/ui/Input/Input";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <Input placeholder={"input text"}  />
+      <Input placeholder={"input text"} />
     </main>
   );
 }
