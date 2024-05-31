67f3d6dc02cbd426c5aaadbe606ef450eacadd47
Evgeniy Trubnikov <parmesanshees@mail.ru>
2024-05-31 11:51:48 +0300

    update: Slider component

--- src/shared/ui/Slider/Slider.tsx
+++ src/shared/ui/Slider/Slider.tsx
@@ -4,6 +4,10 @@ import * as SliderPrimitive from "@radix-ui/react-slider";
 import { cn } from "@/shared/func/utils";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { AnimatePresence } from "framer-motion";
+import dynamic from "next/dynamic";
+
+const MotionDiv = dynamic(() => import("framer-motion").then((mod) => mod.motion.div));
 
 interface ISliderProps {
   tooltip?: boolean;
@@ -16,23 +20,16 @@ const Slider = forwardRef<
   ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & ISliderProps
 >(
   (
-    {
-      className,
-      tooltip,
-      progressBar,
-      textStyle = "dark",
-      value,
-      onValueChange,
-      ...props
-    }, ref,
+    { className, tooltip, progressBar, textStyle = "dark", value, onValueChange, ...props },
+    ref,
   ) => {
     // const [value, setValue] = useState(45);
     const [isHolding, setIsHolding] = useState(false);
 
     const handleValueChange = (newValue: number[]) => {
       if (progressBar) return;
-      if(onValueChange) {
-        onValueChange(newValue)
+      if (onValueChange) {
+        onValueChange(newValue);
       }
     };
 
@@ -50,7 +47,7 @@ const Slider = forwardRef<
       <SliderPrimitive.Root
         ref={ref}
         className={cn(
-          "relative touch-none select-none h-full max-h-[48px] flex w-full items-center",
+          "relative flex h-full max-h-[48px] w-full touch-none select-none items-center",
           className,
         )}
         value={value}
@@ -61,63 +58,90 @@ const Slider = forwardRef<
         {...props}
       >
         <SliderPrimitive.Track
-          className={twMerge("relative w-full grow overflow-hidden rounded-[6px] bg-mint-750", isHolding ? "h-4" : "h-3")}>
+          className={twMerge(
+            "relative w-full grow overflow-hidden rounded-[6px] bg-mint-750 transition-all duration-200",
+            isHolding ? "h-4" : "h-3",
+          )}
+        >
           <SliderPrimitive.Range
-            className={twMerge("absolute h-full bg-yellow-950 rounded-[6px]", isHolding && "rounded-[16px]")} />
+            className={twMerge(
+              "absolute h-full rounded-[6px] bg-yellow-950",
+              isHolding && "rounded-[16px]",
+            )}
+          />
         </SliderPrimitive.Track>
 
         <SliderPrimitive.Thumb
-          className={twMerge("flex items-center justify-center h-12 w-12 rounded-full bg-mint-750 backdrop-blur-sm transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+          className={twMerge(
+            "focus-visible:ring-ring flex h-12 w-12 items-center justify-center rounded-full bg-mint-750 backdrop-blur-sm transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
             progressBar && "hidden",
           )}
         >
-          <div className={"flex items-center justify-center w-[32px] h-[32px] bg-white rounded-full"}>
-            <div className={"w-full h-full flex items-center justify-center bg-gradient-slider rounded-full "}>
-              <div className={"w-[28px] h-[28px] bg-white rounded-full shadow-slider"} />
+          <div className="flex h-[32px] w-[32px] items-center justify-center rounded-full bg-white">
+            <div className="flex h-full w-full items-center justify-center rounded-full bg-gradient-slider">
+              <div className={"h-[28px] w-[28px] rounded-full bg-white shadow-slider"} />
             </div>
           </div>
 
-          {/*tooltip*/}
-          {tooltip && isHolding && <div className={"absolute bottom-[65px] text-[13px] animate-tooltip-appear"}
-                                        style={{ transformOrigin: 'bottom center' }}
-          >
-            <div
-              className={"relative font-bold w-[54px] h-[26px] flex items-center justify-center leading-[19.5px]  rounded-[40px] bg-button-gradient-orange !z-[2]"}>
-              <span className={"text-white text-shadow-red-two"}>{value}%</span>
-            </div>
-            <div
-              className={"absolute bg-[#CB6B39] rounded-[2px] !w-[20px] !h-[15px] left-[35%] top-[14px] -rotate-45 !z-[1]"} />
-          </div>}
-
+          {/* tooltip */}
+          <AnimatePresence>
+            {tooltip && isHolding && (
+              <MotionDiv
+                className="absolute bottom-[65px] origin-bottom text-[13px]"
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0, opacity: 0 }}
+              >
+                <div className="relative !z-[2] flex h-[26px] w-[54px] items-center justify-center rounded-[40px]  bg-button-gradient-orange font-bold leading-[19.5px]">
+                  <span className={"text-white text-shadow-red-two"}>{value}%</span>
+                </div>
+                <div className="absolute left-[35%] top-[14px] !z-[1] !h-[15px] !w-[20px] -rotate-45 rounded-[2px] bg-[#CB6B39]" />
+              </MotionDiv>
+            )}
+          </AnimatePresence>
         </SliderPrimitive.Thumb>
 
-        {/*progress bar*/}
-        {progressBar && <div
-          className={twMerge("absolute top-[27px] w-full text-center  text-[13px] font-bold leading-[19.5px]",
-            textStyle === "dark" ? "!text-brown-700 text-shadow-gold" : "!text-mint-900 text-shadow-light",
-          )}>
-          {value}% done
-        </div>}
+        {/* progress bar */}
+        {progressBar && (
+          <div
+            className={twMerge(
+              "absolute top-[27px] w-full text-center text-[13px] font-bold leading-[19.5px]",
+              textStyle === "dark"
+                ? "!text-brown-700 text-shadow-gold"
+                : "!text-mint-900 text-shadow-light",
+            )}
+          >
+            {value}% done
+          </div>
+        )}
 
-        {/*min max*/}
-        {(props.min || props.max) &&
-          <div className={"w-full flex justify-between absolute top-[27px] text-[13px] font-bold leading-[19.5px]"}>
+        {/* min max */}
+        {(props.min || props.max) && (
+          <div className="absolute top-[27px] flex w-full justify-between text-[13px] font-bold leading-[19.5px]">
             <span
-              className={twMerge(textStyle === "dark" ? "!text-brown-700 text-shadow-gold" : "!text-mint-900 text-shadow-light")}
+              className={twMerge(
+                textStyle === "dark"
+                  ? "!text-brown-700 text-shadow-gold"
+                  : "!text-mint-900 text-shadow-light",
+              )}
             >
               min
             </span>
             <span
-              className={twMerge(textStyle === "dark" ? "!text-brown-700 text-shadow-gold" : "!text-mint-900 text-shadow-light")}
+              className={twMerge(
+                textStyle === "dark"
+                  ? "!text-brown-700 text-shadow-gold"
+                  : "!text-mint-900 text-shadow-light",
+              )}
             >
               max
             </span>
           </div>
-        }
-
+        )}
       </SliderPrimitive.Root>
     );
-  });
+  },
+);
 Slider.displayName = SliderPrimitive.Root.displayName;
 
 export { Slider };
