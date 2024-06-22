be697c50f894de9d5a6ac63f32252e67ca1fdec3
Evgeniy Trubnikov <parmesanshees@mail.ru>
2024-06-22 16:04:15 +0300

    add: icons assets

---
+++ shared/ui/ElementsIcons/index.tsx
@@ -0,0 +1,4 @@
+import AirIcon from "./icons/Air.svg";
+import DirtIcon from "./icons/Dirt.svg";
+
+export { AirIcon, DirtIcon };
---
+++ widgets/Modal/constants/assets.ts
@@ -0,0 +1,8 @@
+import { AirIcon } from "@/shared/ui/ElementsIcons";
+
+export const cardElementsAssets = {
+  air: {
+    icon: AirIcon,
+    type: "elements",
+  },
+};
