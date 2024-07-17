ccdd91687ed879f9955d646b9ad6618c9c1856d1
shuverida <bashtarov@outlook.com>
2024-07-17 15:33:17 +0300

    Added api and types for chat

--- shared/lib/services/chat/index.ts
+++ shared/lib/services/chat/index.ts
@@ -1,5 +1,7 @@
 import { serverApi } from "../../axios";
 import { errorHandler } from "../../utils/error-handler";
+import { Pagination, PostMessageOptions } from "@/shared/lib/types/chat";
+
 
 export namespace ChatService {
   const thisName = (serviceName: string) => {
@@ -7,10 +9,55 @@ export namespace ChatService {
     return `${namespaceName} > ${serviceName}`;
   };
 
-  /**
-   * POST /game-chat/create-game
-   */
+  //POST /game-chat/post-message
+  export const postMessage = (options: PostMessageOptions) => {
+    return errorHandler(thisName("postMessage"), serverApi.post("/game-chat/post-message", options));
+  };
+
+  //GET /game-chat/list/${chatId}
+  export const getMessages = (chatId: string, pagination: Pagination) => {
+    return errorHandler(thisName("getMessages"), serverApi.post(
+        `game-chat/list/${chatId}`,
+        { params: pagination },
+      ),
+    );
+  };
+
+  //POST /game-chat/start-report
+  export const startReportDialog = () => {
+    return errorHandler(thisName("startReportDialog"), serverApi.post("/game-chat/start-report"));
+  };
+
+  //PUT /game-chat/onboarding-report/start
+  export const startOnboardingReport = () => {
+    return errorHandler(thisName("startOnboardingReport"), serverApi.put("/game-chat/onboarding-report/start"));
+  };
+
+  //POST /game-chat/start-practice
+  export const startPracticeDialog = (gameId: string, cellId: number) => {
+    return errorHandler(thisName("startPracticeDialog"), serverApi.post("/game-chat/start-practice", {
+      gameId,
+      cellId,
+    }));
+  };
+
+  //PUT /game-chat/practices/${practiceId}/send-message
+  export const sendPracticeMessage = (practiceId: string) => {
+    return errorHandler(thisName("sendPracticeMessage"), serverApi.put(`/game-chat/practices/${practiceId}/send-message`));
+  };
+
+  //POST /game-chat/start-final-report
+  export const startFinalReportDialog = () => {
+    return errorHandler(thisName("startFinalReportDialog"), serverApi.post("/game-chat/start-final-report"));
+  };
+
+  //POST /game-chat/create-game
   export const createGame = () => {
     return errorHandler(thisName("createGame"), serverApi.post("/game-chat/create-game"));
+  };
+
+  //POST /game-chat/reset-game
+  export const resetGame = () => {
+    return errorHandler(thisName("resetGame"), serverApi.post("/game-chat/reset-game"));
   };
 }
